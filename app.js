c927f61bd8e7eaf12f94edd0b9cca9f76d8ef4eb
MerlingRivas <merlingrivas212@gmail.com>
2022-04-30 00:12:29 -0600

    se agrega let

---
+++ app.js
@@ -0,0 +1,8 @@
+
+
+let equal_pressed =0;
+let button_input=document.querySelectorAll(".input_button");
+
+let Inicio = document.getElementById("Inicio");
+let Acercade = document.getElementById("Acerca de ");
+let Aplicacion = document.getElementById("Aplicacion");
